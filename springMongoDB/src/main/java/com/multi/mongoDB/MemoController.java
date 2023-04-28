defc25065da21e64d7e113b1fec23b5966dd81ae
baloo365 <124110711+baloo365@users.noreply.github.com>
2023-04-28 23:55:25 +0900

    4월 28일 수업
    mongoDB로 메모장 만들기

--- springMongoDB/src/main/java/com/multi/mongoDB/MemoController.java
+++ springMongoDB/src/main/java/com/multi/mongoDB/MemoController.java
@@ -5,10 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller // 싱글톤 + 컨트롤러 등록
+
+@Controller //싱글톤 + 컨트롤러 등록 
 public class MemoController {
 
 	@Autowired
@@ -16,11 +16,38 @@ public class MemoController {
 	
 	@RequestMapping("list.memo")
 	public void list(Model model) {
-		List<MemoVO> list = dao.list();
-		System.out.println(list.size());
-		//dao의 list()해서 결과 받아오면 됨
-		// 결과 받아온 것을 model의 속성을 지정해서 view까지 검색결과를 보내자!
+		List<MemoVO2> list = dao.list(); 
+		System.out.println(list);
 		model.addAttribute("list", list);
 	}
-	
+
+	@RequestMapping("insert.memo") 
+	public void insert(MemoVO2 vo) {
+		System.out.println("test...");
+		dao.insert(vo);
+	}
+
+	@RequestMapping("one.memo")
+	public void one(String _id, Model model) {
+		System.out.println("one.memo컨트롤러 >> " + _id);
+		MemoVO2 vo = dao.one(_id);
+		model.addAttribute("vo", vo);
+	}
+
+	@RequestMapping("update.memo")
+	public String update(String _id, String content, Model model) {
+		//400 error --> bad request error
+		MemoVO2 vo = new MemoVO2();
+		vo.set_id(_id);
+		vo.setContent(content);
+		dao.update(vo);
+		System.out.println("update.memo 컨트롤러>> " + vo);
+		return "redirect:/mongo_memo.jsp";
+	}
+
+	@RequestMapping("delete.memo")
+	public String delete(String _id) {
+		dao.delete(_id);
+		return "redirect:/mongo_memo.jsp";
+	}
 }
